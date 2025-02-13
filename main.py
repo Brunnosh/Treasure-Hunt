abc22d4f1862b0ad3c30748a7b9727c8f174a35b
Bruno Savoia <brunotsavoia@gmail.com>
2025-02-13 08:17:38 -0300

    Movimentação aparentemente funcional

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 
-MAPA = [] # Mapa do tesouro
+MAPA = [] # MAPA do tesouro
 PONTOS_EXPLORADOS = [] # Pontos já visitados
 PONTOS_INCERTOS = []  # Pontos com mais de um caminho possivel
 
-def lermapa ():
-    with open('mapa.txt','r') as file:
+def lerMAPA ():
+    with open('MAPA.txt','r') as file:
         for line in file:
             linha = line.strip()
             temp = []
@@ -29,7 +29,7 @@ def achar_pos_inicial():
     
 
 
-def ler_pos_proximas(posatual,mapa):
+def ler_pos_proximas(posatual):
     #   #
     #  #S#
     #   #
@@ -40,51 +40,66 @@ def ler_pos_proximas(posatual,mapa):
     esq = [posatual[0],posatual[1]-1]
     dir = [posatual[0],posatual[1]+1]
 
-    if mapa[cima[0]][cima[1]] == 'T': return (["TESOURO"],cima)
-    if mapa[baixo[0]][baixo[1]] == 'T': return (["TESOURO"],baixo)
-    if mapa[esq[0]][esq[1]] == 'T': return (["TESOURO"],esq)
-    if mapa[dir[0]][dir[1]] == 'T': return (["TESOURO"],dir)
+    if MAPA[cima[0]][cima[1]] == 'T': return ('TESOURO',cima)
+    if MAPA[baixo[0]][baixo[1]] == 'T': return ('TESOURO',baixo)
+    if MAPA[esq[0]][esq[1]] == 'T': return ('TESOURO',esq)
+    if MAPA[dir[0]][dir[1]] == 'T': return ('TESOURO',dir)
 
-    if mapa[cima[0]][cima[1]] == '.': proxpos.append(cima)
-    if mapa[baixo[0]][baixo[1]] == '.': proxpos.append(baixo)
-    if mapa[esq[0]][esq[1]] == '.': proxpos.append(esq)
-    if mapa[dir[0]][dir[1]] == '.': proxpos.append(dir)
+    if MAPA[cima[0]][cima[1]] == '.': proxpos.append(cima)
+    if MAPA[baixo[0]][baixo[1]] == '.': proxpos.append(baixo)
+    if MAPA[esq[0]][esq[1]] == '.': proxpos.append(esq)
+    if MAPA[dir[0]][dir[1]] == '.': proxpos.append(dir)
 
     #Remover do vetor movimentos possiveis pontos onde ja foi explorado
 
+    print("PONTOS EXPLORADOS: ", PONTOS_EXPLORADOS)
     for movimento in proxpos:
+        print("MOVIMENTO NO PROXPOS: ", movimento)
         if movimento in PONTOS_EXPLORADOS:
             proxpos.remove(movimento)
 
-    if len(proxpos) > 1:
+    if len(proxpos) > 1: # Adiciona a lista de pontos com mais de uma possibilidade de movimento
         PONTOS_INCERTOS.append(posatual)
+
+    if len(proxpos) == 0: # Chegou num ponto onde não tem o que fazer a nao ser retornar
+        return (["SEM SAIDA"])
 
 
     return proxpos
 
-def mover(mapa,prox):
+def mover(prox):
     escolha = random.randrange(len(prox))
     PONTOS_EXPLORADOS.append(prox[escolha])
 
     return prox[escolha]
 
+def voltarUltimoAmbiguo():
+    tam = len(PONTOS_INCERTOS)
+    ultimoValor = PONTOS_INCERTOS[tam-1]
+    PONTOS_INCERTOS.remove(ultimoValor)
+
+    return ultimoValor
+
 
 if __name__ == "__main__":
-    lermapa()
+    lerMAPA()
     posinicial = achar_pos_inicial() # Posição inicial do aventureiro
     posatual = posinicial # Posição atual do aventureiro (inicia na inicial e muda conforme o programa)
     
 
     while True:
+        print("PONTOS EXPLORADOS", PONTOS_EXPLORADOS)
         prox = []
-        prox = ler_pos_proximas(posatual,MAPA)
-        if prox[0] == "TESOURO":
+        prox = ler_pos_proximas(posatual)
+        print(prox)
+        if prox[0] == 'TESOURO':
             print("TESOURO ENCONTRADO EM: ", prox[1])
             exit()
-        posatual = mover(MAPA,prox)
-        print("POSICAO ATUAL",  posatual)
-        print("PONTOS EXPLORADOS:",  PONTOS_EXPLORADOS)
-        print("PONTOS INCERTOS" , PONTOS_INCERTOS)
+        if prox[0] == 'SEM SAIDA':
+            posatual = voltarUltimoAmbiguo()
+        else:
+            posatual = mover(prox)
+        
 
     
     
